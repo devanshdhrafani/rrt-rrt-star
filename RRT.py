e158ffbd76cd72fc55532cf41e8579a909418227
devanshdhrafani <f20171569@hyderabad.bits-pilani.ac.in>
2021-01-31 21:56:41 +0530

    Added step logic.

--- RRT.py
+++ RRT.py
@@ -17,25 +17,23 @@ def main():
     obstacles=graph.makeObs()
     map.drawMap(obstacles)
 
-    
-    tick=0
-    while(tick<500):   
-        x,y = graph.sample_envir()
-        n = graph.number_of_nodes()
-        graph.add_node(n,x,y)
-        graph.add_edge(n-1,n)
-        x1,y1 = graph.x[n],graph.y[n]
-        x2,y2 = graph.x[n-1],graph.y[n-1]
-        graph.add_node(n,x,y)
-        if(graph.isFree()):
-            pygame.draw.circle(map.map,map.Red,(graph.x[n],graph.y[n]),map.nodeRad,map.nodeThickness)
-            if not graph.crossObstacle(x1,x2,y1,y2):
-                pygame.draw.line(map.map,map.Blue,(x1,y1),(x2,y2),map.edgeThickness)
+    while(iteration<500):
+        if iteration%10 == 0:
+            X,Y,Parent = graph.bias(goal)
+            pygame.draw.circle(map.map,map.grey,(X[-1],Y[-1]),map.nodeRad,0)
+            pygame.draw.line(map.map,map.Blue,(X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),map.edgeThickness)
 
-        pygame.display.update()
-        tick=tick+1
+        else:
+            X,Y,Parent = graph.expand()
+            pygame.draw.circle(map.map,map.grey,(X[-1],Y[-1]),map.nodeRad,0)
+            pygame.draw.line(map.map,map.Blue,(X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),map.edgeThickness)
 
-    #pygame.display.update()
+        if iteration % 5 == 0:
+            pygame.display.update()
+
+        iteration=iteration+1
+
+    pygame.display.update()
     pygame.event.clear()
     pygame.event.wait(0)
 
--- RRTbasePy.py
+++ RRTbasePy.py
@@ -163,8 +163,24 @@ class RRTGraph:
             self.add_edge(n1,n2)
             return True
 
-    def step(self):
-        pass
+    def step(self,nnear,nrand,dmax=35):
+        d=self.distance(nnear,nrand)
+        if d>dmax:
+            u=dmax/d
+            (xnear,ynear)=(self.x[nnear],self.y[nnear])
+            (xrand,yrand)=(self.x[nrand],self.y[nrand])
+            (px,py)=(xrand-xnear,yrand-ynear)
+            theta=math.atan2(px,py)
+            (x,y)=(int(xnear+dmax*math.cos(theta)),
+                    int(ynear+dmax*math.sin(theta))
+            )
+            self.remove_node(nrand)
+            if abs(x-self.goal[0])<dmax and abs(y-self.goal[1])<dmax:
+                self.add_node(nrand,self.goal[0],self.goal[1])
+                self.goalstate = nrand
+                self.goalFlag = True
+            else:
+                self.add_node(nrand,x,y)
 
     def path_to_goal(self):
         pass
@@ -172,11 +188,25 @@ class RRTGraph:
     def getPathCoords(self):
         pass
 
-    def bias(self):
-        pass
+    # go towards goal
+    def bias(self,ngoal):
+        n=self.number_of_nodes()
+        self.add_node(n,ngoal[0],ngoal[1])
+        nnear=self.nearest(n)
+        self.step(nnear,n)
+        self.connect(nnear,n)
+        return self.x,self.y,self.parent
 
+    # explore
     def expand(self):
-        pass
+        n=self.number_of_nodes()
+        x,y=self.sample_envir()
+        self.add_node(n,x,y)
+        if self.isFree():
+            xnearest=self.nearest(n)
+            self.step(xnearest,n)
+            self.connect(xnearest,n)
+        return self.x,self.y,self.parent
 
     def cost(self):
         pass
